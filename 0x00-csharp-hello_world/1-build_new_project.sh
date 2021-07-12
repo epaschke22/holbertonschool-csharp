f4e2bc2b25e313bbcde738af1207279188976c17
epaschke22 <ericksonpaschke@gmail.com>
2021-07-12 13:45:26 -0400

    updating executable files

--- 0x00-csharp-hello_world/1-build_new_project.sh
+++ 0x00-csharp-hello_world/1-build_new_project.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 #creates and builds new c# project
 dotnet new console -o 1-new_project
+dotnet build 1-new_project
--- 0x00-csharp-hello_world/2-run_new_project.sh
+++ 0x00-csharp-hello_world/2-run_new_project.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 #creates, builds, and runs new c# project
 dotnet new console -o 2-new_project
+dotnet build 2-new_project
+dotnet run 2-new_project
