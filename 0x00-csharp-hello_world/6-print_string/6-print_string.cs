044848bed667484dcb41c7da40802c02385690a2
epaschke22 <ericksonpaschke@gmail.com>
2021-07-12 14:35:13 -0400

    submitting task 6

--- 0x00-csharp-hello_world/6-print_string/6-print_string.cs
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -6,6 +6,6 @@ class Program
 	{
 		string str = "Holberton School";
         var strfirst = str.Substring(0, 9);
-		Console.WriteLine("{0}{0}{0}\n{1}\n", str, strfirst);
+		Console.WriteLine("{0}{0}{0}\n{1}", str, strfirst);
     }
 }
