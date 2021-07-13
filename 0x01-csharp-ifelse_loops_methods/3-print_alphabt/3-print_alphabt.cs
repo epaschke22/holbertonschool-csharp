357e8bc4a33dadae43887c8297a1ccad0eaf5a63
epaschke22 <ericksonpaschke@gmail.com>
2021-07-13 12:12:46 -0400

    submitting tasks 2 and 3

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _3_print_alphabt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for (i = 97; i < 123; i++) {
+                if (i == 101 || i == 113){
+                    continue;
+                }
+                Console.Write("{0}", (char)i);
+            }
+        }
+    }
+}
