09bcf90861a19aef3d998e90458f87b161e38429
epaschke22 <ericksonpaschke@gmail.com>
2021-07-13 15:04:36 -0400

    Submitting the rest of the project

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintLine(int length)
+    {
+        int i;
+        for (i = length; i > 0; i--) {
+            Console.Write("_");
+        }
+        Console.WriteLine("");
+    }
+}
---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        if (length <= 0){
+            Console.WriteLine("");
+            return;
+        }
+        int rows;
+        int spaces;
+        for (rows = 0; rows < length; rows++){
+            for (spaces = 0; spaces < rows; spaces++){
+                Console.Write(" ");
+            }
+            Console.WriteLine("\\");
+        }
+        Console.WriteLine("");
+    }
+}
