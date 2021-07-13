1c5d1bffb20d1ee41622b94c402a767748f2d1b2
epaschke22 <ericksonpaschke@gmail.com>
2021-07-13 14:38:44 -0400

    submitting task 7

---
+++ 0x01-csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Character
+{
+    public static bool IsLower(char c)
+    {
+        if (c < 96) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
