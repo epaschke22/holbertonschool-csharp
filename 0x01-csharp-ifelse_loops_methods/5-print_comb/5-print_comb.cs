dd4feedeff3f5674d9913f3ec7cba8ac8426222a
epaschke22 <ericksonpaschke@gmail.com>
2021-07-13 12:42:23 -0400

    submitting task 5 and 6

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for (i = 0; i < 100; i++) {
+                Console.Write("{0:00}", i);
+                if (i < 99){
+                    Console.Write(", ");
+                }
+            }
+        }
+    }
+}
---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int j;
+            for (i = 0; i < 9; i++) {
+                for (j = i + 1; j < 10; j++) {
+                    Console.Write("{0}{1}", i, j);
+                    if (i < 8) {
+                        Console.Write(", ");
+                    }
+                }
+            }
+        }
+    }
+}
