2a0a5f8ad2f92588eeab799283dae5f54149e736
epaschke22 <ericksonpaschke@gmail.com>
2021-07-14 15:54:59 -0400

    Update 14-rectangular_array.cs

--- 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -10,7 +10,10 @@ namespace _14_rectangular_array
             grid[2,2] = 1;
             for (int row = 0; row < grid.GetLength(0); row++) {
                 for (int col = 0; col < grid.GetLength(1); col++) {
-                    Console.Write("{0} ", grid[row,col]);
+                    Console.Write("{0}", grid[row,col]);
+                    if (col < grid.GetLength(1) - 1) {
+                        Console.Write(" ");
+                    }
                 }
                 Console.WriteLine("");
             }
