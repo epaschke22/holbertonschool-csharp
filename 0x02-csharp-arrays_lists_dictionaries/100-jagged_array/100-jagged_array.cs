bb9adb730deaf614b04efe88f3475deb88e4c58d
epaschke22 <ericksonpaschke@gmail.com>
2021-07-14 16:16:49 -0400

    submitting advanced

--- 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -6,7 +6,18 @@ namespace _100_jagged_array
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[][] jagged = new int[3][]
+            {
+                new int[] {0, 1, 2, 3},
+                new int[] {0, 1, 2, 3, 4, 5, 6},
+                new int[] {0, 1}
+            };
+            for (int row = 0; row < jagged.Length; row++) {
+                for (int col = 0; col < jagged[row].Length; col++) {
+                    Console.Write("{0}{1}", jagged[row][col], col == (jagged[row].Length - 1) ? "" : " ");
+                }
+                Console.WriteLine("");
+            }
         }
     }
 }
