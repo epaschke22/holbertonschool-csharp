8b6728b01bd803af3bb21d4542ecd7f63d638111
epaschke22 <ericksonpaschke@gmail.com>
2021-07-15 12:06:52 -0400

    Adding files for project

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        var uniqueList = myList.Distinct().ToList();
+        int sum = 0;
+        for (int i = 0; i < uniqueList.Count; i++) {
+            sum += uniqueList[i];
+        }
+        return sum;
+    }
+}
---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/README.md
@@ -0,0 +1,2 @@
+# 0x03. C# - Data Structures: HashSet, Stack, Queue, LinkedList
+https://intranet.hbtn.io/projects/395
