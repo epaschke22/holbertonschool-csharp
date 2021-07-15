6fff0a65100a79ba86dbddd15cb6a8336da9a306
epaschke22 <ericksonpaschke@gmail.com>
2021-07-15 14:06:49 -0400

    Update 6-linkedlist_length.cs

--- 0x03-csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace _6_linkedlist_length
+class LList
 {
-    class Program
+    public static int Length(LinkedList<int> myLList)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        if (myLList == null) {
+            return 0;
         }
+        int count = 0;
+        while (myLList.First.Next != null) {
+            myLList.RemoveFirst();
+            count++;
+        }
+        count++;
+        return count;
     }
 }
