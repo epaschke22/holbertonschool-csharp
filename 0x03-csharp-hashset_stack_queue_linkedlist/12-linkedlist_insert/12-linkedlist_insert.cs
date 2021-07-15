35b7fea934e8988a82c3bc04817c5422e3fd9b71
epaschke22 <ericksonpaschke@gmail.com>
2021-07-15 15:34:51 -0400

    Update 12-linkedlist_insert.cs

--- 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace _12_linkedlist_insert
+class LList
 {
-    class Program
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        LinkedListNode<int> current;
+        current = myLList.First;
+        if (current.Value > n) {
+            myLList.AddFirst(n);
+            return myLList.First;
         }
+        while (current != null)
+        {
+            if (current.Next.Value > n) {
+                myLList.AddAfter(current, n);
+                return current.Next;
+            }
+            current = current.Next;
+        }
+        myLList.AddLast(n);
+        return myLList.Last;
     }
 }
