be119250746fb975f7fda583fdce4d5235b60f55
epaschke22 <ericksonpaschke@gmail.com>
2021-07-29 12:49:34 -0400

    removed double documentation

--- 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
+++ 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
@@ -5,7 +5,6 @@ namespace MyMath
     ///<summary>Adds 2 numbers</summary>
     public class Operations
     {
-        ///<summary>Main function</summary>
         public static int Add(int a, int b)
         {
             return a + b;
