d69018a4c08d8d6a22498cabe812cd81df1e79be
epaschke22 <ericksonpaschke@gmail.com>
2021-08-02 18:52:58 -0400

    added documentation task 3

--- 0x08-csharp-inheritance/3-type_get/3-type_get.cs
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -3,6 +3,7 @@ using System.Reflection;
 
 class Obj
 {
+    ///<summary>prints out all properties and methods of an object</summary>
     public static void Print(object myObj)
     {
         Console.WriteLine("{0} Properties:", myObj.GetType().Name);
