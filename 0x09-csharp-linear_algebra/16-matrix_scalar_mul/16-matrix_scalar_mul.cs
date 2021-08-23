d42cd7397718801fe0a72421fe8e10d10890ca5f
epaschke22 <ericksonpaschke@gmail.com>
2021-08-23 14:06:36 -0400

    Update 16-matrix_scalar_mul.cs

--- 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
+++ 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 
-namespace _16_matrix_scalar_mul
+class MatrixMath
 {
-    class Program
+    ///<summary>multiplies matrix by scalar, 2d or 3d</summary>
+    public static double[,] MultiplyScalar(double[,] matrix, double scalar)
     {
-        static void Main(string[] args)
+        if ((matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2) || (matrix.GetLength(0) == 3 && matrix.GetLength(1) == 3))
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 0; i < matrix.GetLength(0); i++)
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                    matrix[i, j] *= scalar;
+            return matrix;
+        }
+        else
+        {
+            return new double[,] {{-1}};
         }
     }
 }
