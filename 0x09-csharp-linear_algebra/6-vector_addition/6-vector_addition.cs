35392963f30ac938c82375c4877fa8119f9c53c7
epaschke22 <ericksonpaschke@gmail.com>
2021-08-23 12:25:00 -0400

    Update 6-vector_addition.cs

--- 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -1,9 +1,24 @@
 ﻿using System;
 
-class Program
+class VectorMath
 {
-    static void Main(string[] args)
+    ///<summary>adds 2 vectors together, 2d or 3d</summary>
+    public static double[] Add(double[] vector1, double[] vector2)
     {
-        Console.WriteLine("Hello World!");
+        if (vector1.Length == 2 && vector2.Length == 2)
+        {
+            double[] output = new double[] { vector1[0] + vector2[0], vector1[1] + vector2[1] };
+            return output;
+        }
+        else if (vector1.Length == 3 && vector2.Length == 3)
+        {
+            double[] output = new double[] { vector1[0] + vector2[0], vector1[1] + vector2[1], vector1[2] + vector2[2] };
+            return output;
+        }
+        else
+        {
+            double[] output = new double[] {-1};
+            return output;
+        }
     }
 }
