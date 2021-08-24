16ade269e66602bff4a3bc99d146e7b29382bda3
epaschke22 <ericksonpaschke@gmail.com>
2021-08-24 12:15:31 -0400

    Update 20-matrix_rotate_2D.cs

--- 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -10,14 +10,11 @@ class MatrixMath
         }
         double[,] rotation = new double[,] { { Math.Cos(angle), Math.Sin(angle) }, { -Math.Sin(angle), Math.Cos(angle) } };
         double[,] output = new double[2, 2];
-        double total = 0;
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 2; j++) {
-                total = 0;
                 for (int k = 0; k < 2; k++) {
-                    total += Math.Round(matrix[i, k] * rotation[k, j], 2);
+                    output[i, j] = Math.Round(output[i, j] + matrix[i, k] * rotation[k, j], 2);
                 }
-                output[i, j] = total;
             }
         }
         return output;
