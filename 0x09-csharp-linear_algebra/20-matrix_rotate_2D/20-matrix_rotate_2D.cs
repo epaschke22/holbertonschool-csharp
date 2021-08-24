c2eb7998fc4c1d77efa2f16345367a55ccb948de
epaschke22 <ericksonpaschke@gmail.com>
2021-08-24 12:12:58 -0400

    Update 20-matrix_rotate_2D.cs

--- 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -9,29 +9,15 @@ class MatrixMath
             return new double[,] {{-1}};
         }
         double[,] rotation = new double[,] { { Math.Cos(angle), Math.Sin(angle) }, { -Math.Sin(angle), Math.Cos(angle) } };
-        return Multiply(matrix, rotation);
-    }
-
-    ///<summary>multiplies 2 matricies</summary>
-    public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
-    {
-        int M1row = matrix1.GetLength(0);
-        int M1col = matrix1.GetLength(1);
-        int M2row = matrix2.GetLength(0);
-        int M2col = matrix2.GetLength(1);
-        double[,] output = new double[M1row, M2col];
+        double[,] output = new double[2, 2];
         double total = 0;
-        if (M1col != M2row) {
-            return new double[,] {{-1}};
-        } else {
-            for (int i = 0; i < M1row; i++) {
-                for (int j = 0; j < M2col; j++) {
-                    total = 0;
-                    for (int k = 0; k < M1col; k++) {
-                        total += matrix1[i, k] * matrix2[k, j];
-                    }
-                    output[i, j] = Math.Round(total, 0);
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                total = 0;
+                for (int k = 0; k < 2; k++) {
+                    total += Math.Round(matrix[i, k] * rotation[k, j], 2);
                 }
+                output[i, j] = total;
             }
         }
         return output;
