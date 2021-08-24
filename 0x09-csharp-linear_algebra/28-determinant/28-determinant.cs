a13236fd3f816ab38359a8266fd9612b4935c47a
epaschke22 <ericksonpaschke@gmail.com>
2021-08-24 14:42:16 -0400

    Update 28-determinant.cs

--- 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
+++ 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 
-namespace _28_determinant
+class MatrixMath
 {
-    class Program
+    ///<summary>gets a determinant of a matrix, 2D or 3D</summary>
+    public static double Determinant(double[,] matrix)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        if (matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2) {
+            return matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0];
+        } else if (matrix.GetLength(0) == 3 && matrix.GetLength(1) == 3) {
+            double a = matrix[0, 0] * (matrix[1, 1] * matrix[2, 2] - matrix[1, 2] * matrix[2, 1]);
+            double b = matrix[0, 1] * (matrix[1, 0] * matrix[2, 2] - matrix[1, 2] * matrix[2, 0]);
+            double c = matrix[0, 2] * (matrix[1, 0] * matrix[2, 1] - matrix[1, 1] * matrix[2, 0]);
+            return a - b + c;
+        } else {
+            return -1;
         }
     }
 }
