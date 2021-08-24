637963832c0ff885824ed3707c99692a241358ea
epaschke22 <ericksonpaschke@gmail.com>
2021-08-24 16:01:50 -0400

    Update 33-inverse_2D.cs

--- 0x09-csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
+++ 0x09-csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
@@ -4,6 +4,15 @@ class MatrixMath
 {
     public static double[,] Inverse2D(double[,] matrix)
     {
-        
+        if (matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2) {
+            double[,] flipped = new double[2, 2] {{matrix[1, 1], -matrix[0, 1]}, {-matrix[1, 0], matrix[0, 0]}};
+            double determinant = 1 / (matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0]);
+            for (int i = 0; i < 2; i++)
+                for (int j = 0; j < 2; j++)
+                    flipped[i, j] *= determinant;
+            return flipped;
+        } else {
+            return new double[,] {{-1}};
+        }
     }
 }
