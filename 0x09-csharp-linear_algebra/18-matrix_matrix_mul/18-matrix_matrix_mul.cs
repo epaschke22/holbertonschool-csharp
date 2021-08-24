fdf7543a441cdb51528c1fab595c4151ddbdeb9d
epaschke22 <ericksonpaschke@gmail.com>
2021-08-24 09:37:39 -0400

    Update 18-matrix_matrix_mul.cs

--- 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
+++ 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 
-namespace _18_matrix_matrix_mul
+class MatrixMath
 {
-    class Program
+    ///<summary>multiplies 2 matricies</summary>
+    public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        int M1row = matrix1.GetLength(0);
+        int M1col = matrix1.GetLength(1);
+        int M2row = matrix2.GetLength(0);
+        int M2col = matrix2.GetLength(1);
+        double[,] output = new double[M1row, M2col];
+        double total = 0;
+        if (M1col != M2row) {
+            return new double[,] {{-1}};
+        } else {
+            for (int i = 0; i < M1row; i++) {
+                for (int j = 0; j < M2col; j++) {
+                    total = 0;
+                    for (int k = 0; k < M1col; k++) {
+                        total += matrix1[i, k] * matrix2[k, j];
+                    }
+                    output[i, j] = total;
+                }
+            }
         }
+        return output;
     }
 }
