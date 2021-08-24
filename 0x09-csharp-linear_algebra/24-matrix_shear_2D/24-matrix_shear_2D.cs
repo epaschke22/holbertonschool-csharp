b3bafe3ed2079a45cf797a9952d355d869c4b2b6
epaschke22 <ericksonpaschke@gmail.com>
2021-08-24 13:29:08 -0400

    Update 24-matrix_shear_2D.cs

--- 0x09-csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
+++ 0x09-csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 
-namespace _24_matrix_shear_2D
+class MatrixMath
 {
-    class Program
+    public static double[,] Shear2D(double[,] matrix, char direction, double factor)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        if ((matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2) || (direction != 'x' && direction != 'y')) {
+            return new double[,] {{-1}};
         }
+        double[,] rotation = new double[2, 2];
+        if (direction == 'x'){
+            rotation = new double[,] { { 1, 0 }, { factor, 1 } };
+        } else {
+            rotation = new double[,] { { 1, factor }, { 0, 1 } };
+        }
+        double[,] output = new double[2, 2];
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                for (int k = 0; k < 2; k++) {
+                    output[i, j] = Math.Round(output[i, j] + matrix[i, k] * rotation[k, j], 2);
+                }
+            }
+        }
+        return output;
     }
 }
