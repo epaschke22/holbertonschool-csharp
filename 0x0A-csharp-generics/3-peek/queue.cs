196cd88f80a69e8df1adb2d853c5918602270c48
epaschke22 <ericksonpaschke@gmail.com>
2021-10-25 17:06:40 -0400

    Update queue.cs

--- 0x0A-csharp-generics/3-peek/queue.cs
+++ 0x0A-csharp-generics/3-peek/queue.cs
@@ -1,12 +1,82 @@
 ﻿using System;
 
-namespace _3_peek
+///<summary>generic class</summary>
+public class Queue<T>
 {
-    class Program
+    ///<summary>returns the type of the object passed in</summary>
+    public Type CheckType()
     {
-        static void Main(string[] args)
+        return typeof(T);
+    }
+
+    ///<summary>node class for list</summary>
+    public class Node
+    {
+        ///<summary>value in node</summary>
+        public T value = default(T);
+        ///<summary>pointer to next node</summary>
+        public Node next = null;
+
+        ///<summary>node constructor</summary>
+        public void constuctNode(T val)
         {
-            Console.WriteLine("Hello World!");
+            value = val;
         }
+    }
+
+    ///<summary>head of queue list</summary>
+    public Node head;
+    ///<summary>tail of queue list</summary>
+    public Node tail;
+    ///<summary>number of queue list nodes</summary>
+    public int count = 0;
+
+    ///<summary>adds node to queue</summary>
+    public void Enqueue(T val)
+    {
+        Node newNode = new Node();
+        newNode.value = val;
+        if (head == null)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        count += 1;
+    }
+
+    ///<summary>returns number of nodes</summary>
+    public int Count()
+    {
+        return count;
+    }
+
+    ///<summary>removes node to queue</summary>
+    public T Dequeue()
+    {
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return default(T);
+        }
+        T result = head.value;
+        head = head.next;
+        count -= 1;
+        return result;
+    }
+
+    ///<summary>returns value of head node</summary>
+    public T Peek()
+    {
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return default(T);
+        }
+        return head.value;
     }
 }
