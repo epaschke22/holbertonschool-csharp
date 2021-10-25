ac1dfd9d6b277063ebde7ffe5df8b1a1d0c4fc73
epaschke22 <ericksonpaschke@gmail.com>
2021-10-25 15:14:39 -0400

    submitting task 0

---
+++ 0x0A-csharp-generics/0-queue/queue.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+///<summary>generic class</summary>
+public class Queue<T>
+{
+    ///<summary>returns the type of the object passed in</summary>
+    public string CheckType()
+    {
+        return typeof(T).Name;
+    }
+}
