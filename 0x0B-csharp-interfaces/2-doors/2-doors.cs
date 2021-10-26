a146cd979c9985caec5033c51494b26aea30af4d
epaschke22 <ericksonpaschke@gmail.com>
2021-10-26 13:56:56 -0400

    Update 2-doors.cs

--- 0x0B-csharp-interfaces/2-doors/2-doors.cs
+++ 0x0B-csharp-interfaces/2-doors/2-doors.cs
@@ -1,12 +1,48 @@
 ﻿using System;
 
-namespace _2_doors
+///<summary>abstract class</summary>
+public abstract class Base
 {
-    class Program
+    ///<summary>abstract class variable</summary>
+    public string name { get; set; }
+    ///<summary>abstract class method overrid</summary>
+    public override string ToString()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        return String.Format("{0} is a {1}", name, this.GetType());
+    }
+}
+
+///<summary>interface 1</summary>
+interface IInteractive
+{
+    void Interact();
+}
+
+///<summary>interface 2</summary>
+interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
+}
+
+///<summary>interface 3</summary>
+interface ICollectable
+{
+    bool isCollected { get; set; }
+    void Collect();
+}
+
+///<summary>Door Class</summary>
+public class Door : Base, IInteractive
+{
+    ///<summary>constructor</summary>
+    public Door(string Name = "Door")
+    {
+        name = Name;
+    }
+    ///<summary>interface 1 method</summary>
+    public void Interact()
+    {
+        Console.WriteLine($"You try to open the {name}. It's locked.");
     }
 }
