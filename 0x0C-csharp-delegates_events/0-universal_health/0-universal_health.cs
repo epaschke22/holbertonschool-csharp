9519d935397b4c6041cc31c2df9244a758ecca21
epaschke22 <ericksonpaschke@gmail.com>
2021-10-28 14:28:54 -0400

    Update 0-universal_health.cs

--- 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -3,9 +3,9 @@
 ///<summary>Player class</summary>
 public class Player
 {
-    private string name;
-    private float maxHp;
-    private float hp;
+    string name;
+    float maxHp;
+    float hp;
 
     ///<summary>Player Constructor</summary>
     public Player(string name = "Player", float maxHp = 100f)
@@ -14,10 +14,9 @@ public class Player
         if (maxHp <= 0)
         {
             Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
-            this.maxHp = 100f;
+            maxHp = 100f;
         }
-        else
-            this.maxHp = maxHp;
+        this.maxHp = maxHp;
         this.hp = maxHp;
     }
     ///<summary>Print Player health</summary>
