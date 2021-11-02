47cb1984083bedf5ec0da71c3277381eb77b8077
epaschke22 <ericksonpaschke@gmail.com>
2021-11-02 16:35:44 -0400

    update on project

--- 0x0D-csharp-text_based_interface/InventoryLibrary/JSONStorage.cs
+++ 0x0D-csharp-text_based_interface/InventoryLibrary/JSONStorage.cs
@@ -1,10 +1,14 @@
 using System;
+using System.IO;
 using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace InventoryLibrary
 {
     public class JSONStorage
     {
+        string fileName = "./storage/inventory_manager.json";
         Dictionary<string, string> objects = new Dictionary<string, string>();
 
         public Dictionary<string, string> All()
@@ -14,17 +18,19 @@ namespace InventoryLibrary
 
         public void New(BaseClass obj)
         {
-            objects.Add(obj.GetType().Name, obj.id);
+            objects.Add(obj.GetType().Name + '.' + obj.id, obj);
         }
 
         public void Save()
         {
-
+            string jsonString = JsonSerializer.Serialize(objects);
+            File.WriteAllText(fileName, jsonString);
         }
 
         public void Load()
         {
-
+            string jsonString = File.ReadAllText(fileName);
+            objects = JsonSerializer.Deserialize<Dictionary<string, string>>(jsonString);
         }
     }
 }
