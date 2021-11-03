f43265557d49b6692d4a11dc90a514ce0b5243ad
epaschke22 <ericksonpaschke@gmail.com>
2021-11-02 20:11:29 -0400

    Update InventoryManager.cs

--- 0x0D-csharp-text_based_interface/InventoryManager/InventoryManager.cs
+++ 0x0D-csharp-text_based_interface/InventoryManager/InventoryManager.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using InventoryLibrary;
 
 namespace InventoryManager
 {
@@ -6,7 +7,22 @@ namespace InventoryManager
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Inventory Manager");
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("<ClassNames> show all ClassNames of objects");
+            Console.WriteLine("<All> show all objects");
+            Console.WriteLine("<All [ClassName]> show all objects of a ClassNam");
+            Console.WriteLine("<Create [ClassName]> a new object");
+            Console.WriteLine("<Show [ClassName object_id]> an object");
+            Console.WriteLine("<Update [ClassName object_id]> an object");
+            Console.WriteLine("<Delete [ClassName object_id]> an object");
+            Console.WriteLine("<Exit>");
+
+            while (true)
+            {
+                string name = Console.ReadLine();
+                
+            }
         }
     }
 }
